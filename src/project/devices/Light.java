e012a6dfe9862a9df330bb027f56bd859c32f9c3
Yagna123k <yagna82kb@gmail.com>
2024-09-13 10:35:02 +0530

    Used This pointer concept

---
+++ src/project/devices/Light.java
@@ -0,0 +1,27 @@
+package project.devices;
+
+public class Light extends Device {
+    private int brightness;
+
+    public Light(String name, int brightness) {
+        super(name);  
+        this.brightness = brightness;
+    }
+
+    public void adjustBrightness(int newBrightness) {
+        if (isOn) {
+            brightness = newBrightness;
+            System.out.println(name + " brightness adjusted to " + brightness + "%.");
+        } else {
+            System.out.println(name + " is OFF. Cannot adjust brightness.");
+        }
+    }
+
+    @Override
+    public void displayStatus() {
+        super.displayStatus();  
+        if (isOn) {
+            System.out.println("Brightness: " + brightness + "%");
+        }
+    }
+}
